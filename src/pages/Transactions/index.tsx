23110a491e863e3d90e7f2b16ede019176ba157f
sergio almeida <sergioalmeidaa00@gmail.com>
2023-05-01 19:16:25 -0300

    FEAT: add table in pages Home

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,36 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { Price, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions(){
   return (
     <>
       <Header/>
       <Summary/>
+
+      <TransactionContainer>
+        <TransactionTable>
+            <tbody>
+              <tr>
+                <td>Desenvolvimento de site</td>
+                <td>
+                  <Price variant="income"> R$ 12.000,00 </Price>                  
+                </td>
+                <td>Venda</td>
+                <td>13/04/2022</td>
+              </tr>
+              <tr>
+                <td>Aluguel do apartamento</td>
+                <td>
+                  <Price variant="outcome"> - R$ 1.200,00 </Price>                  
+                </td>
+                <td>Casa</td>
+                <td>13/04/2022</td>
+              </tr>
+            </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+
     </>
   )
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const TransactionContainer = styled.div`
+  width: 100%;
+  max-width: 70rem;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`
+
+export const TransactionTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing:0 0.5rem;
+  /* margin-top: 1.5rem; */
+
+  td{
+    padding: 1.25rem 2rem;
+    background: ${({ theme }) => theme.gray[700]};    
+
+    &:first-child{
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child{
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+    
+  }
+`
+interface PriceProps{
+  variant: 'income' | 'outcome'
+}
+export const Price = styled.span<PriceProps>`
+  color: ${({ theme,variant }) => variant === 'income' ? theme.green[300] : theme.red[300]};
+`
